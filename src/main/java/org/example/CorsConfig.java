dcfc2c68d9b7e51ab2a5437edb8fbcba6d2fe5e1
Datong <Datong@example.com>
2025-08-31 15:10:32 +0800

    初始化

--- src/main/java/org/example/CorsConfig.java
+++ src/main/java/org/example/CorsConfig.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,6 +13,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  **/
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+
+
+    @Value("${website.domain}")
+    private String domain;
 
 
     public void addCorsMappings(CorsRegistry registry) {
--- src/main/java/org/example/service/impl/DeepseekServiceImpl.java
+++ src/main/java/org/example/service/impl/DeepseekServiceImpl.java
@@ -9,17 +9,10 @@ import org.example.service.DeepseekService;
 import org.example.utils.SSEMsgType;
 import org.example.utils.SSEServer;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.client.ChatClientResponse;
-import org.springframework.ai.chat.model.ChatResponse;
-import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.deepseek.DeepSeekChatModel;
-import org.springframework.ai.deepseek.DeepSeekChatOptions;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Flux;
 
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Stream;
 
 
 @Slf4j
@@ -33,9 +26,10 @@ public class DeepseekServiceImpl implements DeepseekService {
 
     @Resource
     private DeepSeekChatModel chatModel;
+
     @Override
     public void deepseekv3DoctorStream3(String userName, String message) {
-        chatRecordService.saveChatRecord(userName,message, ChatTypeEnum.USER);
+        chatRecordService.saveChatRecord(userName, message, ChatTypeEnum.USER);
         ChatClient.ChatClientRequestSpec prompt = chatClient.prompt();
         List<String> list = prompt.user(message).stream().chatClientResponse().toStream().map(chatClientResponse -> {
             String text = chatClientResponse.chatResponse().getResult().getOutput().getText();
@@ -43,21 +37,14 @@ public class DeepseekServiceImpl implements DeepseekService {
             return text;
         }).toList();
         System.out.println("list = " + JSON.toJSONString(list));
-
+        SSEServer.sendMessage(userName, "GG", SSEMsgType.FINISH);
         // 保存AI回复的记录到数据库
         String htmlResult = "";
+        htmlResult = "<think>";
         for (String s : list) {
             htmlResult += s;
         }
-        chatRecordService.saveChatRecord(userName,htmlResult, ChatTypeEnum.BOT);
-/*        Flux<ChatResponse> chatResponseFlux = prompt.user(message).stream().chatResponse();
-        List<String> list1 = chatResponseFlux.toStream().map(chatResponse -> {
+        chatRecordService.saveChatRecord(userName, htmlResult, ChatTypeEnum.BOT);
 
-            String text = chatResponse.getResult().getOutput().getText();
-            log.info(text);
-            SSEServer.sendMessage(userName, text, SSEMsgType.ADD);
-            return text;
-        }).toList();*/
-        SSEServer.sendMessage(userName, "GG", SSEMsgType.ADD);
     }
 }
